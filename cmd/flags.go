df74c74b4a07a6bfb469f265f91023028666cf64
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-02-26 06:29:06 +0000

    Fix: type in getFlags description

--- cmd/flags.go
+++ cmd/flags.go
@@ -21,7 +21,7 @@ func getFlags() args {
 	}
 
 	flag.IntVar(&defaults.Autos, "nAutos", 5, "number of autos to run during the simulation")
-	flag.IntVar(&defaults.Delta, "delta", 25, "the amount of passengers to change during stop [rand(-delta,delta)]")
+	flag.IntVar(&defaults.Delta, "delta", 25, "the amount of passengers to change during a stop [rand(-delta,delta)]")
 	flag.IntVar(&defaults.InitialCount, "passengers", 50, "the amount of passengers autos start with")
 	flag.DurationVar(&defaults.Duration, "period", 5*time.Second, "The periodicity of auto stops")
 	flag.Parse()
