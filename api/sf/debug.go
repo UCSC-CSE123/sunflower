c9e13b62813fae80e62ce411f3e1bac452dc63e6
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-03-01 18:12:33 +0000

    Add: Time elapsed to debug info

--- api/sf/debug.go
+++ api/sf/debug.go
@@ -1,10 +1,13 @@
 package sf
 
-import "github.com/UCSC-CSE123/sunflower/pkg/bus"
+import (
+	"github.com/UCSC-CSE123/sunflower/pkg/bus"
+)
 
 type DebugInfo struct {
 	StopPeriodicity string `json:"StopPeriodicity"`
 	InitialCount    int    `json:"InitialCount"`
+	ElapsedTime     string `json:"ElapsedTime"`
 }
 
 type StateWDebug struct {
--- api/sf/state.go
+++ api/sf/state.go
@@ -35,25 +35,41 @@ func ServeInstantChange(numBuses, initialCount, delta int, interval time.Duratio
 // It then updates that state asynchronously every interval according to updateFunc.
 // It returns an http.HandlerFunc that has access to that state.
 func CustomServe(numBuses, initialCount int, interval time.Duration, updateFunc func(*bus.State, *sync.RWMutex)) http.HandlerFunc {
+	// Initialize the read write mutex and the state itself.
 	var mutex sync.RWMutex
 	state := StateWDebug{
 		State: bus.NewState(numBuses, initialCount),
 		Info: DebugInfo{
 			StopPeriodicity: interval.String(),
 			InitialCount:    initialCount,
+			ElapsedTime:     "0s",
 		},
 	}
 
+	// Start a go routine to update the time every second.
+	go func() {
+		var currentTime time.Duration
+		for range time.Tick(1 * time.Second) {
+			currentTime += 1 * time.Second
+			mutex.Lock()
+			state.Info.ElapsedTime = currentTime.String()
+			mutex.Unlock()
+		}
+	}()
+
+	// Start another go routine to update the state.
 	go func() {
 		for range time.Tick(interval) {
 			updateFunc(&state.State, &mutex)
 		}
 	}()
 
+	// Return the function that handles the encoding of the state.
 	return func(w http.ResponseWriter, r *http.Request) {
 		mutex.RLock()
 		defer mutex.RUnlock()
 		w.Header().Set("Content-type", "application/json")
 		json.NewEncoder(w).Encode(state)
 	}
+
 }
